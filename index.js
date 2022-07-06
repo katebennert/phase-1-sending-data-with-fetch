4349c72e5d9eb8356e94fcbb3338f02956ca22ec
Kate Bennert <kate.bennert@gmail.com>
2022-07-06 16:03:25 -0400

    commit tree e3ffac80d0c0dd315b49ba0ecf434691a97e3aad

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch(`http://localhost:3000/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email
+        })
+    })
+    .then(response => response.json())
+    .then(user => {
+        const p = document.createElement("p");
+        const div = document.createElement("div");
+        document.querySelector('body').append(div);
+        p.innerHTML = `${user.id}`;
+        div.append(p);
+    })
+    .catch(errorObj => {
+        const p = document.createElement("p");
+        const div = document.createElement("div");
+        document.querySelector('body').append(div);
+        p.innerHTML = `${errorObj.message}`;
+        div.append(p);
+    })
+}
